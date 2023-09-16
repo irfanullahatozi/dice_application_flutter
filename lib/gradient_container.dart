252e593160bdbebfc73acb5bf21d115bd46c4d75
Irfan Ullah <irfan.ullah@arcelik.com>
2023-09-16 15:02:25 +0500

    adding dice images

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,4 +1,3 @@
-import 'package:first_app/styled_text.dart';
 import 'package:flutter/material.dart';
 
 const startAlignment = Alignment.topLeft;
@@ -12,13 +11,15 @@ class GradientContainer extends StatelessWidget {
   @override
   Widget build(ctx) {
     return Container(
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-            colors: colors, begin: startAlignment, end: endAlignment),
-      ),
-      child: const Center(
-          child: StyledText(
-              "This is just", 30.2, Color.fromARGB(255, 233, 226, 226))),
-    );
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+              colors: colors, begin: startAlignment, end: endAlignment),
+        ),
+        child: Center(
+          child: Image.asset(
+            'assets/images/dice_1.png',
+            width: 200,
+          ),
+        ));
   }
 }
